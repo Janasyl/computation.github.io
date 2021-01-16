8760b47b203cc2e8a78c9489a9aab1533ef566d4
Janasyl <jduishenbaev@gmail.com>
2021-01-16 13:17:39 +0600

    added minus

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ console.log(result1);
 alert(+a + +b);
 
 
+let c = prompt("first number?");
+let d = prompt("second number?");
+let result2 = c - d;
+console.log(result2);
+alert(-c - -d);
+
+
+
+
+
